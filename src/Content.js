4f3a53dad252fe8c5bcb68d5700c664af5b0e4ea
Supreeth-D <Supreeth.D@onsolve.com>
2023-07-11 19:41:32 +0530

    added random name generator

--- src/Content.js
+++ src/Content.js
@@ -1,11 +1,20 @@
 const Content = () => {
+    const handleNameChanges = () => {
+    var Name = ['Kevin','David','Bob'];
+    var randomNum = Math.floor(Math.random() * 3);
+    return Name[randomNum];
+}
     return(
+        <main>
+
+        <span><h3>{handleNameChanges()}</h3><a>Your list is below</a></span>
         <list>
             <ol>Apple</ol>
             <ol>Milk</ol>
             <ol>Bread</ol>
             <ol>Eggs</ol>
         </list>
+        </main>
     );
 }
 
