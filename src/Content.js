0ea929f60032dc8cd2e38b3f0cebbe0892bbc041
Supreeth-D <Supreeth.D@onsolve.com>
2023-07-11 19:11:43 +0530

    Created Content.js file

---
+++ src/Content.js
@@ -0,0 +1,12 @@
+const Content = () => {
+    return(
+        <list>
+            <ol>Apple</ol>
+            <ol>Milk</ol>
+            <ol>Bread</ol>
+            <ol>Eggs</ol>
+        </list>
+    );
+}
+
+export default Content
